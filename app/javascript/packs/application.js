2a3a5ff1e07af6ddd65c3b56cd88cdbcfd90a17d
Satyam <satyam.tripathi@w3villa.com>
2023-03-27 17:31:16 +0530

    error in js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,16 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "bootstrap"
 
-function rentdays(e)
-let day = $('#days').val();
-
-// $("#days").change(function(){    
-//     let day = ($(this).val());
-// });
